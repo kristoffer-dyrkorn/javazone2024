9f9ec0db228dd395357652e14a5e6d44a6e0c1e2
Kristoffer Dyrkorn <>
2024-07-22 20:25:03 +0200

    Use animation loop

--- app/main.js
+++ app/main.js
@@ -2,12 +2,13 @@ import * as THREE from "three"
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js"
 import { OrbitControls } from "three/addons/controls/OrbitControls.js"
 
-// set +Z as up axis everywhere
+// set +Z as our up axis for the entire app
 THREE.Object3D.DEFAULT_UP.set(0, 0, 1)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setAnimationLoop(animate)
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
@@ -19,16 +20,24 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.target.set(1500, 1500, 0)
 controls.update()
 
-const light = new THREE.AmbientLight(1.0)
-scene.add(light)
-
-const directionalLight = new THREE.DirectionalLight(0xddd5cc, 1.0)
+const directionalLight = new THREE.DirectionalLight(0xddd5cc, 2.0)
+directionalLight.position.set(-0.2, -0.4, 0.9)
 scene.add(directionalLight)
+
+const satelliteTexture = new THREE.TextureLoader().load("./andalsnes-satellite.png")
+
+const orthoTexture = new THREE.TextureLoader().load("./andalsnes-ortho.png")
+orthoTexture.colorSpace = THREE.SRGBColorSpace
+
+let terrainMesh
+let buildingMesh
+let roadMesh
 
 const objLoader = new OBJLoader()
 objLoader.load("./andalsnes-terrain.obj", (object) => {
-  scene.add(object)
-  renderer.render(scene, camera)
+  terrainMesh = object.children[0]
+  terrainMesh.material.map = satelliteTexture
+  scene.add(terrainMesh)
 })
 
 window.addEventListener("resize", () => {
@@ -36,9 +45,45 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix()
 
   renderer.setSize(window.innerWidth, window.innerHeight)
-  renderer.render(scene, camera)
 })
 
-controls.addEventListener("change", () => {
-  renderer.render(scene, camera)
+window.addEventListener("keydown", (keyboardEvent) => {
+  switch (keyboardEvent.key) {
+    case "t":
+      if (terrainMesh) {
+        terrainMesh.visible = !terrainMesh.visible
+      }
+      break
+    case "b":
+      if (!buildingMesh) {
+        objLoader.load("./andalsnes-buildings.obj", (object) => {
+          buildingMesh = object.children[0]
+          scene.add(buildingMesh)
+        })
+      } else {
+        buildingMesh.visible = !buildingMesh.visible
+      }
+      break
+    case "r":
+      if (!roadMesh) {
+        objLoader.load("./andalsnes-roads.obj", (object) => {
+          roadMesh = object.children[0]
+          roadMesh.material.color.set(0x555555)
+          scene.add(roadMesh)
+        })
+      } else {
+        roadMesh.visible = !roadMesh.visible
+      }
+      break
+    case "o":
+      terrainMesh.material.map = orthoTexture
+      break
+    case "s":
+      terrainMesh.material.map = satelliteTexture
+      break
+  }
 })
+
+function animate() {
+  renderer.render(scene, camera)
+}
